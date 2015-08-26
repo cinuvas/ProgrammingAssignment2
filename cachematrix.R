4a9eca61ef309ab9b985601c90b3293bb0a5a22e
cinuvas <cinuvas@yahoo.com>
2015-08-26 20:25:47 +0530

    Update cachematrix.R
    Comment Added.

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,24 @@
 
 ## Write a short comment describing this function
 
+## Function set's up the enviroment, to create & hold global variables.
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
+        ##Not been used in this scenario
+        ##To set the input matrix
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        
+        ##Fetch the Intupt matrix  back
         get <- function() x
-		setinv <- function(solve) m <<- solve
-		getinv <- function() m
+        
+        ##Set the Inversed matrix
+        setinv <- function(solve) m <<- solve
+        
+        ##Fetch the Inverse matrix  back
+        getinv <- function() m
         list(set = set, get = get,
 		setinv = setinv,
 		getinv = getinv)
@@ -19,7 +28,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+## This is the function inverse the matrix & cache it for future use.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getinv()
@@ -27,7 +36,9 @@ cacheSolve <- function(x, ...) {
                 message("getting cached data")
                 return(m)
         }
+        ## If the control comes here that means this is the first execution.
         data <- x$get()
+        ##Here is were we are acually inversing the matrix
         m <- solve(data, ...)
         x$setinv(m)
         m
